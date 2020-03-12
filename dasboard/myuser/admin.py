15418729ae6a9f3228ac9c37233555fbd7f72ab6
zakaria100-max <62027885+zakaria100-max@users.noreply.github.com>
2020-03-12 18:56:17 +0100

    Update admin.py

--- dasboard/myuser/admin.py
+++ dasboard/myuser/admin.py
@@ -3,8 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from .models import Benutzer
-
-
+ 
 # Register your models here.
 
 
@@ -20,3 +19,19 @@ class USERAdmin(admin.ModelAdmin):
     list_display= ("email",'first_name', "last_name", "date_joined",'role','is_active' )
     list_filter = ( "last_name", "date_joined",'role','is_active' )
     search_fields = ("date_joined",'role','is_active' )
+    actions = ['aktivieren_status', 'deaktivieren_status']
+    #list_filter = (AlphabetFilter,)
+
+# with this method i get jsut the activ user in the list
+    '''
+    def get_queryset(self, request):
+        return super().get_queryset(request).filter(is_active=True)
+    '''
+
+    def deaktivieren_status(self, request, queryset):
+        return queryset.update(is_active=False)
+    deaktivieren_status.short_description =(" User Deactivieren")
+
+    def aktivieren_status(self, request, queryset):
+        return queryset.update(is_active=True)
+    aktivieren_status.short_description =(" User Aktivieren")

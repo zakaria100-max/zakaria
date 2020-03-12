2c8d8a64f98d3b24b0a1f28198888f7328d7f4b7
MuhammadAhmad902 <muhammad.ahmad@captiq.com>
2020-03-12 16:10:26 +0100

    my Nachricht

--- dasboard/dasboard/settings.py
+++ dasboard/dasboard/settings.py
@@ -23,6 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('BLOG_KEY')
 
+SECRET_KEY = os.environ.get('BLOG_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
